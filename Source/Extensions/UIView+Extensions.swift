128129366b1b111472145dff6cf2855361871ebe
Michael Tai <michael@downloadthebear.com>
2018-10-22 09:38:06 -0400

    - storyboard compatibility

--- Source/Extensions/UIView+Extensions.swift
+++ Source/Extensions/UIView+Extensions.swift
@@ -6,7 +6,7 @@ import Foundation
 @IBDesignable
 extension UIView {
     
-    @IBInspectable
+//    @IBInspectable
     /// Should the corner be as circle
     public var circleCorner: Bool {
         get {
@@ -17,7 +17,7 @@ extension UIView {
         }
     }
     
-    @IBInspectable
+//    @IBInspectable
     /// Corner radius of view; also inspectable from Storyboard.
     public var cornerRadius: CGFloat {
         get {
@@ -29,7 +29,7 @@ extension UIView {
         }
     }
     
-    @IBInspectable
+//    @IBInspectable
     /// Border color of view; also inspectable from Storyboard.
     public var borderColor: UIColor? {
         get {
@@ -47,7 +47,7 @@ extension UIView {
         }
     }
     
-    @IBInspectable
+//    @IBInspectable
     /// Border width of view; also inspectable from Storyboard.
     public var borderWidth: CGFloat {
         get {
@@ -58,7 +58,7 @@ extension UIView {
         }
     }
     
-    @IBInspectable
+//    @IBInspectable
     /// Shadow color of view; also inspectable from Storyboard.
     public var shadowColor: UIColor? {
         get {
@@ -72,7 +72,7 @@ extension UIView {
         }
     }
     
-    @IBInspectable
+//    @IBInspectable
     /// Shadow offset of view; also inspectable from Storyboard.
     public var shadowOffset: CGSize {
         get {
@@ -83,7 +83,7 @@ extension UIView {
         }
     }
     
-    @IBInspectable
+//    @IBInspectable
     /// Shadow opacity of view; also inspectable from Storyboard.
     public var shadowOpacity: Double {
         get {
@@ -94,7 +94,7 @@ extension UIView {
         }
     }
     
-    @IBInspectable
+//    @IBInspectable
     /// Shadow radius of view; also inspectable from Storyboard.
     public var shadowRadius: CGFloat {
         get {
@@ -105,7 +105,7 @@ extension UIView {
         }
     }
     
-    @IBInspectable
+//    @IBInspectable
     /// Shadow path of view; also inspectable from Storyboard.
     public var shadowPath: CGPath? {
         get {
@@ -116,7 +116,7 @@ extension UIView {
         }
     }
     
-    @IBInspectable
+//    @IBInspectable
     /// Should shadow rasterize of view; also inspectable from Storyboard.
     /// cache the rendered shadow so that it doesn't need to be redrawn
     public var shadowShouldRasterize: Bool {
@@ -128,7 +128,7 @@ extension UIView {
         }
     }
     
-    @IBInspectable
+//    @IBInspectable
     /// Should shadow rasterize of view; also inspectable from Storyboard.
     /// cache the rendered shadow so that it doesn't need to be redrawn
     public var shadowRasterizationScale: CGFloat {
@@ -140,7 +140,7 @@ extension UIView {
         }
     }
     
-    @IBInspectable
+//    @IBInspectable
     /// Corner radius of view; also inspectable from Storyboard.
     public var maskToBounds: Bool {
         get {
