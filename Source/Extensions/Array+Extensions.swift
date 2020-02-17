a9732dc4878c04c97c4e4b8f09466a0cfaaba5a0
Andrii Halabuda <andrii.halabuda@gmail.com>
2020-02-17 21:40:45 +0100

    Migrated to Swift 5

--- Source/Extensions/Array+Extensions.swift
+++ Source/Extensions/Array+Extensions.swift
@@ -47,44 +47,44 @@ extension Array where Element: Equatable {
         // Thanks to https://github.com/sairamkotha for improving the method
         return self.reduce([]){ $0.contains($1) ? $0 : $0 + [$1] }
     }
-
+    
     /// Check if array contains an array of elements.
-	///
-	/// - Parameter elements: array of elements to check.
-	/// - Returns: true if array contains all given items.
-	public func contains(_ elements: [Element]) -> Bool {
-		guard !elements.isEmpty else { // elements array is empty
-			return false
-		}
-		var found = true
-		for element in elements {
-			if !contains(element) {
-				found = false
-			}
-		}
-		return found
-	}
-	
-	/// All indexes of specified item.
-	///
-	/// - Parameter item: item to check.
-	/// - Returns: an array with all indexes of the given item.
-	public func indexes(of item: Element) -> [Int] {
-		var indexes: [Int] = []
-		for index in 0..<self.count {
-			if self[index] == item {
-				indexes.append(index)
-			}
-		}
-		return indexes
-	}
-	
-	/// Remove all instances of an item from array.
-	///
-	/// - Parameter item: item to remove.
-	public mutating func removeAll(_ item: Element) {
-		self = self.filter { $0 != item }
-	}
+    ///
+    /// - Parameter elements: array of elements to check.
+    /// - Returns: true if array contains all given items.
+    public func contains(_ elements: [Element]) -> Bool {
+        guard !elements.isEmpty else { // elements array is empty
+            return false
+        }
+        var found = true
+        for element in elements {
+            if !contains(element) {
+                found = false
+            }
+        }
+        return found
+    }
+    
+    /// All indexes of specified item.
+    ///
+    /// - Parameter item: item to check.
+    /// - Returns: an array with all indexes of the given item.
+    public func indexes(of item: Element) -> [Int] {
+        var indexes: [Int] = []
+        for index in 0..<self.count {
+            if self[index] == item {
+                indexes.append(index)
+            }
+        }
+        return indexes
+    }
+    
+    /// Remove all instances of an item from array.
+    ///
+    /// - Parameter item: item to remove.
+    public mutating func removeAll(_ item: Element) {
+        self = self.filter { $0 != item }
+    }
     
     /// Creates an array of elements split into groups the length of size.
     /// If array can’t be split evenly, the final chunk will be the remaining elements.
@@ -109,14 +109,14 @@ extension Array where Element: Equatable {
 public extension Array {
     
     /// Random item from array.
-    public var randomItem: Element? {
+    var randomItem: Element? {
         if self.isEmpty { return nil }
         let index = Int(arc4random_uniform(UInt32(count)))
         return self[index]
     }
     
     /// Shuffled version of array.
-    public var shuffled: [Element] {
+    var shuffled: [Element] {
         var arr = self
         for _ in 0..<10 {
             arr.sort { (_,_) in arc4random() < arc4random() }
@@ -125,18 +125,18 @@ public extension Array {
     }
     
     /// Shuffle array.
-    public mutating func shuffle() {
+    mutating func shuffle() {
         // https://gist.github.com/ijoshsmith/5e3c7d8c2099a3fe8dc3
         for _ in 0..<10 {
             sort { (_,_) in arc4random() < arc4random() }
         }
     }
-
+    
     /// Element at the given index if it exists.
     ///
     /// - Parameter index: index of element.
     /// - Returns: optional element (if exists).
-    public func item(at index: Int) -> Element? {
+    func item(at index: Int) -> Element? {
         guard index >= 0 && index < count else { return nil }
         return self[index]
     }
